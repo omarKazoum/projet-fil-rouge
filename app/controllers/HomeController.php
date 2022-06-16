7e3d9706d496ad3e0b340c7e717da5c58319cee9
omar kazoum <omarKazoum96@gmail.com>
2022-06-16 09:56:44 +0100

    improved signup validation

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -21,64 +21,86 @@ class HomeController
         }
     }
     function sign_up(){
-        viewNoSidebar('sign_up');
+        if(SessionManager::getInstance()->isLoggedIn()) {
+            redirect('/services');
+        }else {
+            viewNoSidebar('sign_up');
+        }
     }
     function signupSubmit(){
-        //TODO:: add validation do not forget the role
-        //let's validate user input
-        if(
-            InputValidator::validateUserName($_POST[FIRST_NAME_KEY],FIRST_NAME_KEY)
-            AND InputValidator::validateUserName($_POST[LAST_NAME_KEY],LAST_NAME_KEY)
-            AND InputValidator::validateUserName($_POST[USER_NAME_KEY],USER_NAME_KEY)
-            AND InputValidator::validateEmail($_POST[EMAIL_KEY],EMAIL_KEY)
-            AND InputValidator::validatePassword($_POST[PASSWORD_KEY],PASSWORD_KEY)
-            AND InputValidator::validatePasswordsMatch($_POST[PASSWORD_KEY],$_POST[PASSWORD_REPEAT_KEY],PASSWORD_REPEAT_KEY)
-            /*AND($_POST[ROLE_KEY]==ROLE_TYPE_COIFFEUR InputValidator::validatePhone($_POST[PHONE_KEY],PHONE_KEY)
-            AND InputValidator::validateCity($_POST[CITY_KEY],CITY_KEY)
-            AND InputValidator::validateAdress($_POST[ADRESS_KEY],ADRESS_KEY)
-            AND InputValidator::validateRole($_POST[ROLE_KEY],ROLE_KEY)
-            AND ($_POST[ROLE_KEY]==ROLE_TYPE_CUSTOMER
-                AND InputValidator::validateWorkingDays($_POST[WORKING_DAYS_KEY],WORKING_DAYS_KEY)
-                AND InputValidator::validateWorkingHours($_POST[WORKING_HOURS_KEY],WORKING_HOURS_KEY))*/){
-            // if the data is valid
-            $user=new User();
-            $user->first_name=$_POST[FIRST_NAME_KEY];
-            $user->last_name=$_POST[LAST_NAME_KEY];
-            $user->user_name=$_POST[USER_NAME_KEY];
-            $user->email=$_POST[EMAIL_KEY];
-            $user->password=$_POST[PASSWORD_KEY];
-            $user->role=$_POST[ROLE_KEY];
-            //TODO::handle image upload here
-            $user->save();
-            $img_path = upload_image(false, PROFILE_IMG_KEY);
-            $img_path=$img_path ? $img_path : IMG_NOT_UPLOADED_KEY;
-            if($user->role==ROLE_TYPE_CUSTOMER){
-                $user->img=$img_path;
-                $c=new Customer();
-                $c->user_id=$user->id;
-                $c->phone=$_POST[PHONE_KEY];
-                $c->img=$img_path;
-                $c->save();
-            }elseif ($user->role==ROLE_TYPE_COIFFEUR){
-                $coiffeur=new Coiffeur();
-                $coiffeur->user_id=$user->id;
-                $coiffeur->city=$_POST[CITY_KEY];
-                $coiffeur->quartier=$_POST[ADRESS_KEY];
-                $coiffeur->phone=$_POST[PHONE_KEY];
-                $coiffeur->store_name=$_POST[STORE_NAME_KEY];
-                $coiffeur->working_hours=$_POST[WORKING_HOURS_KEY];
-                $coiffeur->working_days=$_POST[WORKING_DAYS_KEY];
-                $coiffeur->img=$img_path;
-                $coiffeur->save();
-            }
-            //TODO redirect to index page
-            SessionManager::getInstance()->login($user);
-            redirect('/');
+        $erors=InputValidator::getErrors();
+        if(SessionManager::getInstance()->isLoggedIn()) {
+            redirect('/services');
+        }else {
+              if (InputValidator::validateRole($_POST[ROLE_KEY], ROLE_KEY)
+                and InputValidator::validateName($_POST[FIRST_NAME_KEY], FIRST_NAME_KEY, 'Votre prénom')
+                and InputValidator::validateName($_POST[LAST_NAME_KEY], LAST_NAME_KEY, 'Votre nom')
+                and InputValidator::validateName($_POST[USER_NAME_KEY], USER_NAME_KEY, 'Votre nom d\'utilisateur')
+                and InputValidator::validateEmail($_POST[EMAIL_KEY], EMAIL_KEY)
+                and InputValidator::validatePassword($_POST[PASSWORD_KEY], PASSWORD_KEY)
+                and InputValidator::validatePasswordsMatch($_POST[PASSWORD_KEY], $_POST[PASSWORD_REPEAT_KEY], PASSWORD_REPEAT_KEY)
+                and InputValidator::validateImageType(PROFILE_IMG_KEY, PROFILE_IMG_KEY)
+                and (
+                    (
+                        $_POST[ROLE_KEY] == ROLE_TYPE_COIFFEUR
+                        && InputValidator::validatePhone($_POST[PHONE_KEY], PHONE_KEY)
+                        && InputValidator::validateCity($_POST[CITY_KEY], CITY_KEY)
+                        && InputValidator::validateQuartier($_POST[QUARTIER_KEY], QUARTIER_KEY)
+                        && InputValidator::validateName($_POST[STORE_NAME_KEY], STORE_NAME_KEY, 'Votre nom de salon')
+                        && InputValidator::validateWorkingDays($_POST[WORKING_DAYS_KEY], WORKING_DAYS_KEY)
+                        && InputValidator::validateWorkingHours($_POST[WORKING_HOURS_KEY], WORKING_HOURS_KEY)
+                    )
+                    or
+                    (
+                        $_POST[ROLE_KEY] == ROLE_TYPE_CUSTOMER
+                        && InputValidator::validatePhone($_POST[PHONE_KEY], PHONE_KEY)
+                    )
+                )
+            ) {
+                //if the data is valid
+                //we create a new record in the users table
+                $user = new User();
+                $user->first_name = $_POST[FIRST_NAME_KEY];
+                $user->last_name = $_POST[LAST_NAME_KEY];
+                $user->user_name = $_POST[USER_NAME_KEY];
+                $user->email = $_POST[EMAIL_KEY];
+                $user->password = $_POST[PASSWORD_KEY];
+                $user->role = $_POST[ROLE_KEY];
+                $user->save();
+                //the image is optional so there is a default value
+                $img_path = upload_image(false, PROFILE_IMG_KEY);
+                $img_path = $img_path ? $img_path : IMG_NOT_UPLOADED_KEY;
+                if ($user->role == ROLE_TYPE_CUSTOMER) {
+                    //if the user is a customer we create a new record in the customers table
+                    $user->img = $img_path;
+                    $c = new Customer();
+                    $c->user_id = $user->id;
+                    $c->phone = $_POST[PHONE_KEY];
+                    $c->img = $img_path;
+                    $c->save();
+                } elseif ($user->role == ROLE_TYPE_COIFFEUR) {
+                    //if the user is a coiffeur we create a new record in the coiffeurs table
+                    $coiffeur = new Coiffeur();
+                    $coiffeur->user_id = $user->id;
+                    $coiffeur->phone = $_POST[PHONE_KEY];
+                    $coiffeur->city = $_POST[CITY_KEY];
+                    $coiffeur->quartier = $_POST[QUARTIER_KEY];
+                    $coiffeur->store_title = $_POST[STORE_NAME_KEY];
+                    $coiffeur->work_days = $_POST[WORKING_DAYS_KEY];
+                    $coiffeur->work_hours = $_POST[WORKING_HOURS_KEY];
+                    $coiffeur->img = $img_path;
+                    $coiffeur->save();
+                }
+                //TODO redirect to index page
+                SessionManager::getInstance()->login($user);
+                redirect(getBaseUrlWithMessage('services', 'Vous êtes bien inscrit', 'success'));
+            } else
+                viewNoSidebar('sign_up');
         }
-        else
-            viewNoSidebar('sign_up');
     }
-    function login(){
+    function login(){if(SessionManager::getInstance()->isLoggedIn()) {
+        redirect('/services');
+    }else
         viewNoSidebar('login');
     }
     function loginSubmit(){
