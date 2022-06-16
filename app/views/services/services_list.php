32c57ff25434e2ac565940e0efdd0d945940fa39
omar kazoum <omarKazoum96@gmail.com>
2022-06-16 23:04:58 +0100

    added hours and days in reservation modal

--- app/views/services/services_list.php
+++ app/views/services/services_list.php
@@ -3,16 +3,19 @@
         <div class="page-title col-9 col-md-10">
         Services
         </div>
-        <?php if(\core\SessionManager::getInstance()->getLoggedInUser()->role!=ROLE_TYPE_CUSTOMER){ ?>
+        <div class="col-3 col-md-2">
+        <?php if(\core\SessionManager::getInstance()->getLoggedInUser()->role==ROLE_TYPE_CUSTOMER){ ?>
             <a href="<?= getUrlFor('services/add')?>" class="s-btn primary wrap col-3 col-md-2">
-                <i class="fa fa-plus"></i>Add Service
-            </a>
+                <i class="fa fa-plus"></i>Add Service</a>
         <?php }?>
+        </div>
     </div>
-    </div>
+    <?php require_once dirname(__FILE__) .'/../templates/search_header.php'?>
+
     <div class="row p-1">
         <?php printMessageIfSet();?>
     </div>
+    <div class="row">
         <div class="table-responsive">
         <table class="table table-responsive">
         <?php
@@ -26,55 +29,67 @@
                 <th scope="col">price</th>
                 <th scope="col">category</th>
                 <th scope="col">nom de coiffeur</th>
+                <th scope="col">Demandes faites</th>
                 <th scope="col"></th>
-
             </tr>
         </thead>
         <tbody>
         <?php  foreach($services as $service){ ?>
-        <tr class="service-item">
-        <td >
-            <img style="width:100px;height:80px" src="<?=$service->img!=SERVICE_IMG_NOT_UPLOADED_KEY?uploaded($service->img):img('service_img_place_holder.png') ?>" alt="<?= $service->title?>">
-        </td>
-        <td class="service-title"><?= $service->title ?></td>
-        <td><?= $service->description ?></td>
-        <td><?= $service->price ?>MAD</td>
-        <td><?= $service->category->title ?></td>
-        <td><?= $service->user->user_name ?></td>
-        <td>
-            <div class="action-btns-wrapper">
-                <div class="action-btns">
-                            <!-- supprimer-->
-                            <?php if(\core\SessionManager::getInstance()->getLoggedInUser()->role!=ROLE_TYPE_CUSTOMER){ ?>
-                                <a href="<?= getUrlFor('services/update/'.$service->id)?>" class="s-btn primary p-1 d-flex justify-content-center align-items-center" title="Modifier ce service"><i class="fa fa-edit"></i>Modifier</a>
-                            <?php } ?>
-                            <?php if($service->serviceRequests->count()<1){ ?>
-                                <a href="<?= getUrlFor('services/delete/'.$service->id) ?>" class="s-btn danger p-1 d-flex justify-content-center align-items-center confirm" title="Supprimer ce service"><i class="fa fa-trash" ></i>Supprimer</a>
-                            <?php }
-                           if(!$service->serviceRequests->where('client_id',core\SessionManager::getInstance()->getLoggedInUser()->id)->count()){ ?>
-                                <a  href="#" class="s-btn success p-1 d-flex justify-content-center align-items-center reserve-btn" data-service-id="<?= $service->id?>"><i class="fa fa-shopping-cart" title="Réserver ce service"></i>Réserver</a>
-                           <?php } else{ ?>
-                                <div class="p-1">
-                                    Réservé
-                                </div>
-                    <?php } ?>
-                </div>
-            </div>
-        </td>
+            <tr class="service-item"
+                data-service-hours='<?= $service->coiffeur->work_hours ?>'
+                data-service-days='<?= $service->coiffeur->work_days  ?>'
+            >
+            <td >
+                <img style="width:100px;height:80px" src="<?=$service->img!=IMG_NOT_UPLOADED_KEY?uploaded($service->img):img('service_img_place_holder.png') ?>" alt="<?= $service->title?>">
+            </td>
+            <td class="service-title"><?= $service->title ?></td>
+            <td><?= $service->description ?></td>
+            <td><?= $service->price ?> MAD</td>
+            <td><?= $service->category->title ?></td>
+            <td><?= $service->user->user_name ?></td>
+            <td><?= $service->serviceRequests->count() ?></td>
+            <td>
+                <div class="action-btns-wrapper">
+                    <div class="action-btns">
+                                <!-- supprimer-->
+                                <?php if(\core\SessionManager::getInstance()->getLoggedInUser()->role!=ROLE_TYPE_CUSTOMER){ ?>
+                                    <a href="<?= getUrlFor('services/update/'.$service->id)?>" class="s-btn primary p-1 d-flex justify-content-center align-items-center" title="Modifier ce service"><i class="fa fa-edit"></i>Modifier</a>
+                                <?php } ?>
+                                <?php if($service->serviceRequests->count()<1 AND \core\SessionManager::getInstance()->getLoggedInUser()->role!=ROLE_TYPE_CUSTOMER){ ?>
+                                    <a href="<?= getUrlFor('services/delete/'.$service->id) ?>" class="s-btn danger p-1 d-flex justify-content-center align-items-center confirm" title="Supprimer ce service"><i class="fa fa-trash" ></i>Supprimer</a>
+                                <?php }?>
+                                <?php if(\core\SessionManager::getInstance()->getLoggedInUser()->role==ROLE_TYPE_CUSTOMER){ ?>
 
-        </tr>
+                                    <a  href="#" class="s-btn success p-1 d-flex justify-content-center align-items-center reserve-btn" data-service-id="<?= $service->id?>"><i class="fa fa-shopping-cart" title="Réserver ce service"></i>Réserver</a>
+                                    <?php } ?>
+                    </div>
+                </div>
+            </td>
+
+            </tr>
         <?php } ?>
         </tbody>
-        <?php } ?>
+        <?php }else{?>
+            <div class="alert alert-info">
+                <strong>info:</strong>Aucun service trouvé !
+            <div>
+            <?php } ?>
         </table>
+    </div>
     </div>
     <div class="modal fade bd-example-modal-sm" id='service-modal' tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-sm">
-        <div class="modal-content">
-            <div class="modal-header">
-                 <h5 class="modal-title" id="exampleModalLabel">Réserver le service "<span id="service-title"></span>"</h5>
-            </div>
-            <div class="modal-body">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Réserver le service "<span id="service-title"></span>"</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-success">
+                        <strong>info:</strong>Ce service est diponible:<br>
+                        <span id="modal-service-days">Les </span>
+                        <span id="modal-service-hours"></span>
+
+                    </div>
                     <form onsubmit="return false" action="<?= getUrlFor('services/reserve')?>" method="post">
                         <input type="hidden" name="service_id" id="service_id">
                         <div class="form-group">
@@ -91,18 +106,40 @@
                                 Please select a time.
                             </div>
                         </div>
-                        <?php $service->serviceRequests->where('client_id',\core\SessionManager::getInstance()->getLoggedInUser()->id)?>
                         <button class="s-btn primary reserve-btn">Réserver</button>
                         <a onclick="$('#service-modal').modal('hide');" class="s-btn primary">Cancel</a>
                     </form>
-        </div>
-        </div>
+                </div>
+            </div>
         </div>
     </div>
     <script>
-       document.querySelectorAll('.service-item .reserve-btn').forEach(function(btn) {
+        // if the reserv btn is clicked we will prevent the default action and show the modal after we get the service data
+        document.querySelectorAll('.service-item .reserve-btn').forEach(function(btn) {
            btn.addEventListener('click', function (e) {
                e.preventDefault();
+               // get the service data and show it in the modal
+               const item=e.target.closest('.service-item');
+               console.log("item",item);
+               let serviceDaysFromItem=item.dataset.serviceDays;
+               let serviceHoursFromItem=item.dataset.serviceHours;
+               console.table("time",serviceHoursFromItem);  //debug
+               console.table("days",serviceDaysFromItem);  //debug
+               const hoursInModal=document.querySelector('#modal-service-hours');
+               console.log("hoursInModal",hoursInModal);  //debug
+               const daysInModal=document.querySelector('#modal-service-days');
+               console.log("daysInModal",daysInModal);  //debug
+               //let 's loop all days and insert them in the modal
+               JSON.parse(serviceDaysFromItem).forEach(function(day){
+                   daysInModal.innerHTML+=`<span class="badge badge-secondary">${getDayFromInt(day)}</span>`;
+               });
+                //let 's loop all hours and insert them in the modal
+               let hoursTextArray=[];
+               JSON.parse(serviceHoursFromItem).forEach(function(hoursInterval){
+                   hoursTextArray.push(`<span class="badge badge-primary">${hoursInterval[0]} à ${hoursInterval[1]}</span>`);
+                  });
+               hoursInModal.innerHTML='De '+hoursTextArray.join('<br> Ou De');
+               //let's display the modal
                $('#service-modal').modal('show');
                console.log(e.target.dataset);
                document.querySelector('#service-modal #service_id').value = e.target.dataset.serviceId;
@@ -118,6 +155,7 @@
        document.querySelector("#service-modal .reserve-btn").addEventListener('click',function(e) {
            console.log("reserve btn clicked");
            e.preventDefault();
+           //prepare the form data
            let form = document.querySelector('#service-modal form');
            if(form.checkValidity()) {
                console.log("form is valid");
@@ -151,5 +189,16 @@
                console.log("form is not valid");
            }
         });
+       /**
+        * @param dayInt{int}
+        * @return {string}
+        */
+       const getDayFromInt=(dayInt)=>{
+           let days=['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
+           if(dayInt>=0 && dayInt<7){
+               return days[dayInt];
+           }else
+                console.error('dayInt is not valid:'+dayInt);
+       }
     </script>
 </div>
